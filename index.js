35b23a280a11070160cda77d10e83e76d9c60350
EaseWay Hu <easeway@gmail.com>
2013-05-29 11:28:25 +0800

    use js-class

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = {
-    Class:      require('./lib/Class'),
     States:     require('./lib/States'),
     Trace:      require('./lib/Trace'),
     Config:     require('./lib/Config'),
