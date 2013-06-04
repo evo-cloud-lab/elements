56f00a4cc8afd52666477560080ea8c6199c30af
EaseWay Hu <easeway@gmail.com>
2013-06-04 18:03:52 +0800

    Rewrite Config and add Logger

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    Logger:     require('./lib/Logger'),
     States:     require('./lib/States'),
     Trace:      require('./lib/Trace'),
     Config:     require('./lib/Config'),
